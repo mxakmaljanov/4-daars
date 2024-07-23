58b9965e00fa3ddef65d52ffa997ce7e4ddc9c99
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-07-23 19:01:50 +0500

    4---dars

---
+++ index.js
@@ -0,0 +1,151 @@
+//3-misol
+//et counter=0;
+//or(let i = 4;i<=50;i++){
+//   counter =0;
+//
+//   for(let j=1;j<=i;j++){
+//       if(i%j==0){
+//           counter++
+//       }
+//   }
+//   if (counter>2){
+//       console.log(i);
+//   }
+//
+//5-misol
+//let num= +prompt("sonni kiriting")
+//let max=num;
+//let min=num;
+//for(let i=1; i<=4;i++){
+//    num= +prompt("sonni kiriting")
+//    if(num>max){
+//        max=num
+//    }
+//    if(num<min ){
+//        min=num
+//    }
+//}
+//7- misol
+//let num = 1234
+//let res=0;
+//while(num>=){
+//    res=res-10+num%10
+//    num=Math.trunc(num/10)
+//}
+//console.log(res);
+//1-misol
+//let num=10;
+//let sum=0;
+//for(let i=10;i<=100;i++){
+//    if(i%2==1){
+//        sum=sum+i
+//    }
+//
+//}
+//2-misol
+//let num= +prompt("sonni kiriting")
+//let p=1;
+//for(let i=1;i <= num;i++){
+//    p*=i
+//}
+//4-misol
+//et sum=0;
+//or(let i=1;i<=20;i++){
+//   sum+=i **2
+//6-misol
+//let counter=0;
+//or(let i = -2;i<=30;i++){
+//   counter =0;
+//
+//   for(let j=1;j<=i;j++){
+//       if(i%j==0){
+//           counter++
+//       }
+//   }
+//   if (counter>2){
+//       console.log(i);
+//   }
+// 8-misol
+//let num=10;
+//let sum=0;
+//for(let i=1;i<=10;i++){
+//    if (i%2==0){
+//        sum+=i**2
+//    }else if(i%2==1){
+//        sum+=i**3
+//    }
+//}9-misol
+//let num=50;
+//for(let i=1;i<=50;i++){
+//    if(i%3==0&& i%5==0){
+//
+//    }
+//}
+//console.log(i);
+//10-misol
+//let num=100;
+//let sum
+//for(let i=1;i<=100;i++){
+//    sum+=i/i
+//}
+//12-misol
+//et num= +prompt("sonni kiriting")
+//or(let i=1;i<=num;i++){
+//   if(i/-1){
+//
+//   }
+//
+//onsole.log(i);
+//14-misol
+//let num=200;
+//let sum=0;
+//for(let i=1;i<=200;i++){
+//    sum=sum+i
+//}15-misol
+//let num=20
+//let sum=0;
+//for(let i=1;i<=20;i++){
+//    sum=sum+i
+//}
+//break/continue
+//1-misol
+//let num=10
+//for(let i=1;i<=10;i++){
+//    if(i==5){
+//        break;
+//    }
+//}
+//console.log(i);
+//2-misol
+//let num=10
+//for(let i=1;i<=10;i++){
+//    if(i==3 && i==7){
+//        continue;
+//    }
+//}
+//console.log(i);
+//3-misol
+//let num=10
+//for(let i=1;i<=10;i++){
+//    if(i==7){
+//        break;
+//console.log("topildi");
+//    }
+//}
+//console.log(i);
+//4-misol
+//let num=10
+//for(let i=1;i<=10;i++){
+//    if(i%2==0){
+//        continue;
+//    }
+//}
+//console.log(i);
+//5-misol
+//let num=100
+//for(let i=1;i<=100;i++){
+//    if(i<50){
+//        break;
+//    }
+//}
+//console.log(i);
